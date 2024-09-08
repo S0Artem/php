f988a424202cd3e42213935b3d5247707a6ff8e1
S0Artem <so.artem998@gmail.com>
2024-09-08 15:38:16 +0300

    request переделал

--- php/index.php
+++ php/index.php
@@ -1,30 +1,10 @@
 <?php
+include 'request.php';
+//ролверка поелй
+$request = new Request($_POST['name'], 0, $_POST['tel']);
+$request->create();
 
-$file = 'data.tsv'; // обращаемся к файлу
 
-$current = file_get_contents($file); // считываем
 
-$lines = explode("\r\n", trim($current));
-
-$linesId = end($lines);
-
-$int = explode(" ", $linesId);
-$id = $int[2];
-
-$id = (int)$id;
-
-$x = $id + 1;
-
-$statys = 0;
-
-if (!isset($_POST['name']) || !isset($_POST['tel']) || $_POST['name'] === null || $_POST['tel'] === null) {
-    echo "Записей нету"; // Используйте echo для вывода сообщения
-    exit;
-}
-else{
-    $current .= $_POST['name'] . " " . $_POST['tel'] . " " . $x . " " . $statys . "\r\n"; // добавляем данные
-}
-
-file_put_contents($file, $current); // записываем данные
-header('Location: ../index.html');  // Обновите путь к вашей главной странице
+header('Location: ../index.html', true, 302);
 exit;
--- php/request.php
+++ php/request.php
@@ -1,49 +1,39 @@
 <?php
-class Id
-{
-    function id()
-    {
-        $file = file('data.tsv', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);//читаем файл в массив построчно
-        if (!$file) {
-            return 1;//если пустой возвращаем 1
-        }
-
-        while ($line = end($file)) {//пока не дойдем до конца файла
-            $str = explode(" ", $line);
-            if (isset($str[0], $str[1], $str[2], $str[3]) && is_string($str[0]) && is_numeric($str[1]) && is_numeric($str[2]) && is_numeric($str[3])) {
-                return (int)$str[2] + 1;//елси есть все переменные и они совпадают с нужными хракатеристиками то прошлый id +1
-            }
-            array_pop($file);//если нет удаляем но только у нас, мы это не сохроняем в общую базу(возможно еще млжно востановить строку с данными и тд.) строку
-        }
-        return 1;//если прошли весь файл и не нашли то 1
-    }
-}
-
-
 class Request
 {
     public string $name;
     public int $status;
-    public int $tel;
+    public int $phone;
     public int $id;
-    function __construct($name, $status, $tel, $id = null)
+
+    public function __construct($name, $status, $phone, $id = null)
     {
         $this->name = $name;
         $this->status = $status;
-        $this->tel = $tel;
-        $this->id = $id;
+        $this->phone = $phone;
+        $this->id = $this->genId();
     }
-    function record()
+
+    function create()
     {
-        $current = file_get_contents('data.tsv');
-        $newLine = $this->name . " " . $this->tel . " " . $this->id . " " . $this->status;
-        $current .= "\r\n" . $newLine;
-        file_put_contents('data.tsv', $current);
-        header('Location: ../index.html');
-        exit;
+        $newLine = $this->name . "\t" . $this->phone . "\t" . $this->id . "\t" . $this->status . PHP_EOL;
+        file_put_contents('data.tsv', $newLine, FILE_APPEND);
+    }
+
+    private function genId()
+    {
+        $idFileName = 'id.tsv';
+        assureFileExistance($idFileName);
+
+        $id = (int)file_get_contents($idFileName) + 1;
+        file_put_contents($idFileName, $id);
+
+        return $id;
     }
 }
-$newId = new Id();
-$id = $newId->id();
-$request = new Request($_POST['name'], 0, $_POST['tel'], $id);
-$request->record();
+
+function assureFileExistance(string $filename): void {//убрать
+    if (!file_exists($filename)) {
+        fclose(fopen($filename, 'w'));
+    }
+}
